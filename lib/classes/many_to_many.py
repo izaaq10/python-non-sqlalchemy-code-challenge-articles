7e3e03aa5f349196043e16f9430dd9edc96420a2
Izaaq <odongoizaaq@gmail.com>
2024-06-12 01:57:37 -0400

    commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -9,6 +9,7 @@ class Author:
     def name(self):
         return self._name
 
+
     def add_article(self, magazine, title):
         if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
             raise ValueError("Title must be a string between 5 and 50 characters.")
@@ -30,65 +31,28 @@ class Author:
 
 
 class Magazine:
-    _all_magazines = []
-
     def __init__(self, name, category):
-        if not isinstance(name, str) or len(name) < 2 or len(name) > 16:
-            raise ValueError("Name must be a string between 2 and 16 characters.")
-        if not isinstance(category, str) or len(category) == 0:
-            raise ValueError("Category must be a non-empty string.")
-        self._name = name
+        self.name = name
         self._category = category
-        self._articles = []
-        Magazine._all_magazines.append(self)
-
-    @property
-    def name(self):
-        return self._name
 
     @property
     def category(self):
         return self._category
 
-    def add_article(self, author, title):
-        if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
-            raise ValueError("Title must be a string between 5 and 50 characters.")
-        article = Article(author, self, title)
-        self._articles.append(article)
-        return article
-
-    def articles(self):
-        return self._articles
-
-    def contributors(self):
-        authors = {}
-        for article in self._articles:
-            authors[article.author.name] = authors.get(article.author.name, 0) + 1
-        return [author for author, count in authors.items() if count > 2]
-
-    @classmethod
-    def top_publisher(cls):
-        if not cls._all_magazines:
-            return None
-        return max(cls._all_magazines, key=lambda magazine: len(magazine._articles))
-
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Category must be a non-empty string.")
+        self._category = value
 
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
-        if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
-            raise ValueError("Title must be a string between 5 and 50 characters.")
-        self._author = author
-        self._magazine = magazine
-        self._title = title
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        self.all.append(self)
 
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def author(self):
-        return self._author
-
-    @property
-    def magazine(self):
-        return self._magazine
+    def __repr__(self):
+        return f"Article(author={self.author}, magazine={self.magazine}, title='{self.title}')"
